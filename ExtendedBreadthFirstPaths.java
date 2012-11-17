39b4cf99eebcafb23f61e72292507d1a21b3ef81
Þorgeir Auðunn Karlsson <thorgeirk11@ru.is>
2012-11-17 01:14:44 +0000

    Not final
    Might work

--- ExtendedBreadthFirstPaths.java
+++ ExtendedBreadthFirstPaths.java
@@ -1,11 +1,11 @@
-public class ExtendedBreadthFirstPaths  {
+public class ExtendedBreadthFirstPaths {
     private int numOfPaths[];
     private int levels[];
     private int mostPathsVertex;
     private int mostPaths;
     private int base;
     private Graph g;
-    
+
     public ExtendedBreadthFirstPaths(Graph G, int s) {
         g = G;
         base = s;
@@ -14,50 +14,45 @@ public class ExtendedBreadthFirstPaths  {
         mostPathsVertex = -1;
         mostPaths = -1;
     }
-    
-    private void generate(){
+
+    private void generate() {
         Queue<Integer> q = new Queue<Integer>();
         for (int v = 0; v < g.V(); v++) numOfPaths[v] = Integer.MAX_VALUE;
+        for (int v = 0; v < g.V(); v++) levels[v] = Integer.MAX_VALUE;
         numOfPaths[base] = 1;
         q.enqueue(base);
         levels[base] = 0;
         int level = 0;
-        
         while (!q.isEmpty()) {
             int v = q.dequeue();
             int sumPaths = 0;
-            level = levels[v];
             for (int w : g.adj(v)) {
                 if (numOfPaths[w] != Integer.MAX_VALUE) {
-                    if(levels[w] != level)sumPaths += numOfPaths[w];
-                }
-                else {
+                    if (levels[w] != levels[v]) sumPaths += numOfPaths[w];
+                } else {
                     q.enqueue(w);
-                    levels[w] = level+1;
-               }
+                    if (levels[w] == Integer.MAX_VALUE) levels[w] = levels[v] + 1;
+                }
             }
-           
-            
-            if(sumPaths > mostPaths) {
+            if (sumPaths > mostPaths) {
                 mostPaths = sumPaths;
                 mostPathsVertex = v;
             }
-
-            if(v == base) numOfPaths[v] = 1;
-            else numOfPaths[v] = sumPaths;       
+            if (v == base) numOfPaths[v] = 1;
+            else numOfPaths[v] = sumPaths;
         }
-        
+
     }
-    
-    public int getMostPaths(){
-        if(mostPaths == -1){
+
+    public int getMostPaths() {
+        if (mostPaths == -1) {
             generate();
         }
         return mostPaths;
     }
-    
-    public int getMostPathsVertex(){
-        if(mostPathsVertex == -1){
+
+    public int getMostPathsVertex() {
+        if (mostPathsVertex == -1) {
             generate();
         }
         return mostPathsVertex;
