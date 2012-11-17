9e3cfbeb6d942a3eb9a8374e66084a3bd0ed9240
geirmj11 <geirmj11@ru.is>
2012-11-17 15:44:57 +0000

    Commented SymbolCentrality.java

--- SymbolCentrality.java
+++ SymbolCentrality.java
@@ -17,7 +17,11 @@ public class SymbolCentrality {
     private int center;
     private int popularVertex;
 
-    // Constructor
+    /* Constructor, creates all the arrays and values neccessary and fills the values with '-1'. 
+    * This value is used to indicate that something has not been used. The default zero is sufficient for the arrays, 
+    * since the calculated values will almost never return a zero, and if it is, the cost of calulating is neglible.
+    * BreathFirstPaths is initialized as an array of BreathFirstPaths, one for each node. 
+    */
     public SymbolCentrality(SymbolGraph G) {
         sg = G;
         g = G.G();
@@ -27,29 +31,23 @@ public class SymbolCentrality {
         closeness = new double[g.V()];
         effEcc = new int[g.V()];
 
-        Arrays.fill(effEcc, -1);
-        Arrays.fill(closeness, -1);
-        Arrays.fill(ecc, -1);
-        Arrays.fill(degrees, -1);
         popularVertex = -1;
         center = -1;
         closest = -1;
         effCenter = -1;
     }
 
-    /*
-    * The first four methods compute the centrality of a given node v, for each of the centrality definitions 
-    * (except betweenness).  The last four methods return the index of the vertex with optimal centrality     
-    * value, for each of the four centrality definitions. Ties should be broken in favor of vertices of smallest 
-    * index.
-    */
-
     public int degree(String key) {
         return degree(sg.index(key));
     }
-
+    
+    /* The degree method checks if the degree for a given node has already been calculated,
+    * and if it hasn't it calculates it and stores in the degree array.
+    * It then returns the value from the array for the corresponding node.
+    * If it has been calculated before, the time it takes is constant.
+    */
     public int degree(int v) {
-        if (degrees[v] == -1) {
+        if (degrees[v] == 0) {
             int count = 0;
             for (int i : g.adj(v)) count++;
             degrees[v] = count;
@@ -58,11 +56,16 @@ public class SymbolCentrality {
 
     }
 
+    /* popularVertex checks if popularity has been calculated before, and if so returns that number.
+    *  Otherwise it calculates the degree for all vertices
+    *  that have not previously been calculated, and keeps track of the highest popularity, 
+    *  and the index of the highest. It then returns the most popular one. 
+    */
     public int popularVertex() {
         int popular = -1;
         if (popularVertex == -1) {
             for (int i = 0; i < g.V(); i++) {
-                if (degrees[i] == -1) degree(i);
+                if (degrees[i] == 0) degree(i);
                 if (degrees[i] > popular) {
                     popular = degrees[i];
                     popularVertex = i;
@@ -75,9 +78,14 @@ public class SymbolCentrality {
     public int ecc(String key) {
         return ecc(sg.index(key));
     }
-
+    
+    /* ecc checks if the eccentricity of a given node has been calculated before, and if so, returns that number.
+    * Otherwise it creates a BreathFirstPath for the given node, and finds the highest distance to every node
+    * and keeps track of the highest distance, and stores every distance larger than the currently found in the 
+    * ecc[] array, in the spot allocated to 'v'. After going through all nodes, it returns the value for the given node. 
+    */ 
     public int ecc(int v) {
-        if (ecc[v] == -1) {
+        if (ecc[v] == 0) {
             if (bfp[v] == null) bfp[v] = new BreadthFirstPaths(g, v);
             for (int i = 0; i < g.V(); i++) {
                 if (bfp[v].distTo(i) > ecc[v])
@@ -87,11 +95,15 @@ public class SymbolCentrality {
         return ecc[v];
     }
 
+    /* The center function checks if the center has been calculated before,
+    * and if so, returns that value. If, on the other hand, it has not been calculated
+    * the method goes through the array of eccentricity and finds the node with the lowest value.
+    */ 
     public int center() {
         int lowest = Integer.MAX_VALUE;
         if (center == -1) {
             for (int i = 0; i < g.V(); i++) {
-                if (ecc[i] == -1) ecc(i);
+                if (ecc[i] == 0) ecc(i);
                 if (ecc[i] < lowest) {
                     lowest = ecc[i];
                     center = i;
@@ -101,12 +113,19 @@ public class SymbolCentrality {
         return center;
     }
 
+    /* the closeness method uses the key of the actor to call the closeness method, and returns it's value. */
     public double closeness(String key) {
         return closeness(sg.index(key));
     }
 
+    /* The closeness method checks if the closeness of the given node has been calculated, 
+    *  and if so returns the value from the closeness[] array corresponding to the given node.
+    *  Othewise it creates a breathfirstpath for the given node, if neccessary, and uses that
+    *  goes through all paths in that collection and adds to the hollywood distsance. 
+    *  It then uses the hollywood distance to calculate the closeness.
+    */
     public double closeness(int v) {
-        if (closeness[v] == -1) {
+        if (closeness[v] == 0) {
             if (bfp[v] == null) bfp[v] = new BreadthFirstPaths(g, v);
             double hollywood = 0;
             for (int i = 0; i < g.V(); i++)
@@ -116,11 +135,16 @@ public class SymbolCentrality {
         return closeness[v];
     }
 
+    /* closest checks if the closest node has already been found, and if so, returns that value
+    *  otherwise it calculates the closeness of all nodes, and stores them in the closeness array for each node.
+    *  at the same time it keeps track of the highest value, and the node with the highest value of closeness
+    *  and returns that node.
+    */
     public int closest() {
         if (closest == -1) {
             double high = Double.NEGATIVE_INFINITY;
             for (int i = 0; i < g.V(); i++) {
-                if (closeness[i] == -1) closeness(i);
+                if (closeness[i] == 0) closeness(i);
                 if (closeness[i] > high) {
                     high = closeness[i];
                     closest = i;
@@ -130,12 +154,19 @@ public class SymbolCentrality {
         return closest;
     }
 
+    /* this methods calls the effEcc method with the key for the actor in question. */
     public double effEcc(String key) {
-        return closeness(sg.index(key));
+        return effEcc(sg.index(key));
     }
 
+    /* effEcc checks once more if the effective eccentricity has been calculated, and if so 
+    *  simply returns that value. Otherwise it creates an arraylist to keep track of eccentricitys,
+    *  calculates a breathfirstpath for the given node, and adds all distances in the bfp to the list.
+    *  It then sorts the list, and gets the value at the 90% position in that array,
+    *  stores it in the correct location in the effEcc[] array and returns this value.
+    */
     public int effEcc(int v) {
-        if (effEcc[v] == -1) {
+        if (effEcc[v] == 0) {
             ArrayList<Integer> effs = new ArrayList<Integer>();
             if (bfp[v] == null) bfp[v] = new BreadthFirstPaths(g, v);
 
@@ -151,11 +182,16 @@ public class SymbolCentrality {
         return effEcc[v];
     }
 
+    /* effCenter checks if the effective center of the graph has been caluclated, 
+    *  and if so, returns the value. Otherwise it goes through the list of effective eccentricties
+    *  and calculates for all missing values, keeping track of the minimum value. It then returns 
+    *  the node with the lowest value.
+    */
     public int effCenter() {
         if (effCenter == -1) {
             double min = Double.POSITIVE_INFINITY;
             for (int i = 0; i < g.V(); i++) {
-                if (effEcc[i] == -1) effEcc(i);
+                if (effEcc[i] == 0) effEcc(i);
                 if (effEcc[i] < min) {
                     min = effEcc[i];
                     effCenter = i;
@@ -165,38 +201,18 @@ public class SymbolCentrality {
         return effCenter;
     }
 
-    public void dislpay() {
-        StdOut.println("          Node  Deg  Ecc  Eff   Clo");
-        System.out.printf("Popular:  %3d  %3d  %3d  %3d   %5.3f\n", popularVertex(), degree(popularVertex()), ecc(popularVertex()), effEcc(popularVertex()), closeness(popularVertex()));
-        System.out.printf("Center:   %3d  %3d  %3d  %3d   %5.3f\n", center(), degree(center()), ecc(center()), effEcc(center()), closeness(center()));
-        System.out.printf("Eff.ctr:  %3d  %3d  %3d  %3d   %5.3f\n", effCenter(), degree(effCenter()), ecc(effCenter()), effEcc(effCenter()), closeness(effCenter()));
-        System.out.printf("Closest : %3d  %3d  %3d  %3d   %5.3f\n", closest(), degree(closest()), ecc(closest()), effEcc(closest()), closeness(closest()));
-    }
-
-    public void dislpay(String key) {
-        StdOut.println(key + "\n           Deg  Ecc  Eff   Clo");
-        System.out.printf("Popular:  %3d  %3d  %3d   %5.3f\n\n", degree(sg.index(key)), ecc(sg.index(key)), effEcc(sg.index(key)), closeness(sg.index(key)));
+    public void display(String key) {
+        System.out.printf("%20s: Deg: %3d Ecc: %3d Eff: %3d Clo: %5.3f\n",key, degree(sg.index(key)), ecc(sg.index(key)), effEcc(sg.index(key)), closeness(sg.index(key)));
     }
 
     public static void main(String[] args) {
-        /*
-        * Example output output tiny.out produces on the sample input tiny.txt. 
-        *   
-          Salter, Nicholas
-                     Deg  Ecc  Eff   Clo
-          Popular:    1   12    8   0.158
-        *
-        * The format should match this sample output.  (Specifically, use printf() with formatting
-        * fields “%5.3f“ and “%3d“).
-        */
         SymbolGraph SG = new SymbolGraph(args[0], "/");
         SymbolCentrality cen = new SymbolCentrality(SG);
-        //cen.dislpay(SG.name(0));
-        cen.dislpay("Bacon, Kevin");
-        cen.dislpay("Eastwood, Clint");
-        cen.dislpay("Damon, Matt");
-        cen.dislpay("Spacey, Kevin (I)");
-        cen.dislpay("Salter, Nicholas");
+        cen.display("Bacon, Kevin");
+        cen.display("Eastwood, Clint");
+        cen.display("Damon, Matt");
+        cen.display("Spacey, Kevin (I)");
+        cen.display("Salter, Nicholas");
     }
 
 }
